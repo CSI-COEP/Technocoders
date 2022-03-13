b47205968f615f76ead4bb1ec31de1b026f2a1b6
avd151 <deshpandeav19.comp@coep.ac.in>
2022-03-13 09:55:24 +0530

    minor changes

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class ContractorUser(models.Model):
     image = models.FileField(null=True)
     company = models.CharField(max_length=30, null=True)
     #todo: verification document part left 
+    amount = models.FloatField(max_length=20, null=True)
     type = models.CharField(max_length=15, null=True)
     def _str_(self):
         return self.user.username
